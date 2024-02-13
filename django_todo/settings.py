703d704057f522a597ba123e253b4e0227956d91
jane mckenna <janemckenna@hotmail.com>
2024-02-13 17:47:59 +0000

    Add development variable

--- django_todo/settings.py
+++ django_todo/settings.py
@@ -25,10 +25,13 @@ import dj_database_url
 if os.path.exists("env.py"):
     import env
 
+
+development = os.environ.get('DEVELOPMENT', False)
+
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = development
 
 ALLOWED_HOSTS = ['jbk-django-firstapp-d1718f15ffc0.herokuapp.com']
 
@@ -80,16 +83,17 @@ WSGI_APPLICATION = 'django_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-DATABASES = {
+if development:
+    DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+else:
+    DATABASES = {
     'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
-}
+    }
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
